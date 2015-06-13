ae4c70c7a16d09304ebc56ab868e075985f94216
1amageek <tmy0x3@icloud.com>
2015-06-13 14:06:21 +0900

    Initial Commit

---
+++ PaperKit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PaperKit
+//
+//  Created by Norikazu on 2015/06/13.
+//  Copyright (c) 2015年 Stamp inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
