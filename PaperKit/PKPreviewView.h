482b4c8b4ec2427e8ffe5abf50021f8410f05138
1_am_a_geek <tmy0x3@icloud.com>
2015-07-28 09:55:00 +0900

    add PKPreviewView

---
+++ PaperKit/PKPreviewView.h
@@ -0,0 +1,13 @@
+//
+//  PKPreviewView.h
+//  PaperKit
+//
+//  Created by Norikazu on 2015/07/28.
+//  Copyright © 2015年 Stamp inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PKPreviewView : UIScrollView
+
+@end
---
+++ PaperKit/PKPreviewView.m
@@ -0,0 +1,15 @@
+//
+//  PKPreviewView.m
+//  PaperKit
+//
+//  Created by Norikazu on 2015/07/28.
+//  Copyright © 2015年 Stamp inc. All rights reserved.
+//
+
+#import "PKPreviewView.h"
+
+@implementation PKPreviewView
+
+
+
+@end
